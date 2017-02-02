f09f16dfbaa634f218e930239df948c1c10acb77
aidan <aidan.dodds@googlemail.com>
2017-02-02 10:54:38 +0000

    Convert to CMake project

--- src/samtalk/main.c
+++ src/samtalk/main.c
@@ -3,9 +3,9 @@
 #include <ctype.h>
 #include <string.h>
 
-#include "reciter.h"
-#include "sam.h"
-#include "debug.h"
+#include "../samlib/reciter.h"
+#include "../samlib/sam.h"
+#include "../samlib/debug.h"
 
 #ifdef USESDL
 #include <SDL.h>
@@ -261,9 +261,3 @@ int main(int argc, char **argv)
 	return 0;
 
 }
-
-
-
-
-
-
